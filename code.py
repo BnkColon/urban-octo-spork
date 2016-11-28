812a92d55a191f153d102d0a712780830fdd8a2e
Bianca Colon <colon.bianca@gmail.com>
2016-11-27 21:41:59 -0400

    This code calculate k permutations of a given list

--- code.py
+++ code.py
@@ -3,11 +3,21 @@
 
 # Input: Array of distinct characters (e.g. [’a’,’c’,’d’]) and an integer k
 # Output: list of all possible strings consisting of exactly k characters from the input
+import itertools
+import math
 
-num_array = list()
-num = raw_input("Enter how many elements you want:")
-print 'Enter numbers in array: '
-for i in range(int(num)):
-    n = raw_input("num :")
-    num_array.append(int(n))
-print 'ARRAY: ',num_array
+def possibleStrings(word, integer):
+	for p in itertools.product(word, repeat=integer):
+		print p
+
+if __name__ == '__main__':
+	num_array = ['a', 'b', 'c'] # List of characters
+	integer = 3
+	word = '' #String containing all the characters in num_array
+
+	# Create a string with the characters
+	for c in num_array:
+		word += c
+
+	possibleStrings(word, integer)
+	

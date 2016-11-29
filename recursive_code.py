e9a0ed71942ea98963d631790da0b9573cd535f6
Bianca Colon <colon.bianca@gmail.com>
2016-11-28 20:20:13 -0400

    Program without external modules

---
+++ recursive_code.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Input: Array of distinct characters (e.g. [’a’,’c’,’d’]) and an integer k
+# Output: list of all possible strings consisting of exactly k characters from the input
+
+import sys
+
+integer = int(sys.argv[1])
+array = sys.argv[2:]
